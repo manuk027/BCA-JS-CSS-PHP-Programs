a846d63bb4715ce9591e3b8c61c41f641ae2aadb
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-01 07:30:41 -0800

    Update Display.php

--- 18/Display.php
+++ 18/Display.php
@@ -5,10 +5,10 @@
       mysql_select__db("inv");
       $query="select * from products";
       echo "<center>"
-      while($row=,ysql_fetch_array($result)){
+      while($row=,mysql_fetch_array($result)){
         echo "$row[0]";
+        echo "$row[1]";
         echo "$row[2]";
-        echo "$row[3]";
       }
       echo "</center>";
       <input type="submit" name="stock" value="Back to Home" onclick = "window.location.href = 'Inventory.html';">

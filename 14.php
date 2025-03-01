914c723434e648808579471cdbb2b25db09a5737
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-01 10:33:02 +0530

    Update 14.php

--- 14.php
+++ 14.php
@@ -4,19 +4,19 @@
       Fibonacci Series Generator  
     </h1>
     <form method="POST">
-      <label type="term">Enter number of terms</label>
-      <input type=="number" name="terms" min='1'>
+      <label for="terms">Enter number of terms</label>
+      <input type="number" name="terms" min="1">
       <button type="submit">Generate</button>
     </form>
     <?php
-      if($_SERVER['REQUEST_METHOD']=="POST"){
-        $terms=(int)$_POST['terms'];
-        $fibonacci=array(0, 1);
-        for ($i=2; $i<$terms; $i++){
-          $fibonacci[$i]=$fibonacci[$i-1]+$fibonacci[$i-2];
+      if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $terms = (int)$_POST['terms'];
+        $fibonacci = array(0, 1);
+        for ($i = 2; $i < $terms; $i++){
+          $fibonacci[$i] = $fibonacci[$i-1] + $fibonacci[$i-2];
         }
         foreach($fibonacci as $i){
-          echo $i "<br>";
+          echo $i . "<br>";
         }
       }
     ?>

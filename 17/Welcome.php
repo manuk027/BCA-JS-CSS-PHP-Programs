d316bc7c0d467b91d42a1dd8c96c3fa019c99e9f
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-01 11:43:30 +0530

    Create Welcome.php

---
+++ 17/Welcome.php
@@ -0,0 +1,25 @@
+<htmml>
+  <body>
+      <?php    
+        $u = $_POST['txtname'];
+        $p = $_POST['txtpassword'];
+        mysql_connect("localhost", "root", "tiger");
+        mysql_select_db("inventory");
+        $query = select * from users where username='$u' & passwordd = '$p'";
+        $result = mysql_query($query);
+        if(mysql_affected_rows()==1){
+          $row == mysql_fetch_row($result);
+          $n = row[0];
+          session_start();
+          $_session['name'] = $n;
+          echo "Hello!";
+          echo $sesssion['name'];
+          echo "<br>";
+          session_destroy();
+        }
+        else{
+          echo "Invalid login details";
+        }
+      ?>
+  </body>
+</htmml>

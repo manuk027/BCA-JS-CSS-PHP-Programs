6261ad07deb44b02c2fd4aad1a984416acbda22c
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-01 12:21:42 +0530

    Create Inserthandle.php

---
+++ 18/Inserthandle.php
@@ -0,0 +1,20 @@
+<html>
+  <body>
+    <?php
+      mysql_connect("localhost", "root", "tiger");
+      mysql_select_db("inv");
+      $id = $_POST['txtid'];
+      $item = $_POST['txtitem'];
+      $price = $_POST['txtprice'];
+      $query = "insert into product values('$id', '$item', '$price')";
+      $result = mysql_query($query);
+      if(mysql_affected_rows()==1){
+        echo"<h3>out information has been successfully updated</h3>";
+      }
+      else{
+        echo "<h3>Data cannot be added</h3>";
+      }
+    ?>
+    <input type="submit" name="back" value="back" onclick = "window.location.href = 'Inventory.html';">
+  </body>
+</html>

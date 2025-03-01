439e336fcc2e0ad59d118ad9c6d6e7dc4f63ee55
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-01 10:31:15 +0530

    Update 14.php

--- 14.php
+++ 14.php
@@ -1,1 +1,24 @@
-
+<html>
+  <body>
+    <h1>
+      Fibonacci Series Generator  
+    </h1>
+    <form method="POST">
+      <label type="term">Enter number of terms</label>
+      <input type=="number" name="terms" min='1'>
+      <button type="submit">Generate</button>
+    </form>
+    <?php
+      if($_SERVER['REQUEST_METHOD']=="POST"){
+        $terms=(int)$_POST['terms'];
+        $fibonacci=array(0, 1);
+        for ($i=2; $i<$terms; $i++){
+          $fibonacci[$i]=$fibonacci[$i-1]+$fibonacci[$i-2];
+        }
+        foreach($fibonacci as $i){
+          echo $i "<br>";
+        }
+      }
+    ?>
+  </body>
+</html>

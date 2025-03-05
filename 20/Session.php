d87505ff029b10f48b4e7304e971637befcd0af2
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-05 21:05:45 +0530

    Update Session.php

--- 20/Session.php
+++ 20/Session.php
@@ -7,11 +7,11 @@
   $p = $_POST['txtpassword'];
   mysql_connect("localhost", "root", "tiger");
   mysql_selectdb("Inventory");
-  $query = "selecct * from members where username='$u' & passwword = '$p';";
-  $result= mysql_query($query);
+  $query = "SELECT * FROM members WHERE username='$u' AND password = '$p'";
+  $result = mysql_query($query);
   $nos = mysql_num_rows($result);
   if($nos == 1){
-    echo "$u, you are successfully logged-in";
+    echo "$u, you have successfully logged-in";
   }
   else{
     echo "Access denie";

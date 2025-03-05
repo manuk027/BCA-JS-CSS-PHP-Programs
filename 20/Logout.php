76ba3dfe69e3a20a3c6d876adbeb05c5271f2807
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-05 21:03:33 +0530

    Update Logout.php

--- 20/Logout.php
+++ 20/Logout.php
@@ -1,8 +1,8 @@
 <?php
-  session_start90;
+  session_start();
   unset($_SESSION['txtname']);
   unset($_SESSION['txtpassword']);
-  echo "you have cleared your session";
+  echo "You have cleared your session.";
   echo "Redirecting in 5 seconds....";
-  header("refresh:4:url=Loginsession.html");
+  header("refresh:4;url=Loginsession.html");
 ?>

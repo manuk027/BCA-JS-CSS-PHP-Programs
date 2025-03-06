a7578d1aa66b79e6d34c5c4a2e81285df2348030
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-06 18:48:43 +0530

    Update Result.php

--- 15/Result.php
+++ 15/Result.php
@@ -15,7 +15,7 @@
         echo "<tr><td colspan="2" align="center"><b><u>Marks</u></b></td></tr>";
         echo "<tr><td>ASP:</td><td>{$row[2]}</td></tr>";
         echo "<tr><td>OS:</td><td>{$row[3]}</td></tr>";
-        echo "<tr><td>Network:</td><td>{$row[4]}</td></tr>";
+        echo "<tr><td>NK:</td><td>{$row[4]}</td></tr>";
         echo "<tr><td>Grade:</td><td>{$row[5]}</td></tr>";
       }
       echo "</tables>";

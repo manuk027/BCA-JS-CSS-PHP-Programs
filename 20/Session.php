9ba22fdba6e9ba83094a36ad6ed686906f08fe4e
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-06 19:07:06 +0530

    Update Session.php

--- 20/Session.php
+++ 20/Session.php
@@ -3,11 +3,11 @@
   session_start();
 ?>
 <?php
-  $u = $_POST['txtname'];
-  $p = $_POST['txtpassword'];
+  $u = $_POST[ 'txtname' ];
+  $p = $_POST[ 'txtpassword' ];
   mysql_connect("localhost", "root", "tiger");
   mysql_selectdb("Inventory");
-  $query = "SELECT * FROM members WHERE username='$u' AND password = '$p'";
+  $query = "SELECT * FROM members WHERE username = '$u' AND password = '$p'";
   $result = mysql_query($query);
   $nos = mysql_num_rows($result);
   if($nos == 1){

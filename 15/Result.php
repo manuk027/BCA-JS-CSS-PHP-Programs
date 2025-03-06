f66ac879068e361e527689b466f6448e32aee55f
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-06 18:31:41 +0530

    Update Result.php

--- 15/Result.php
+++ 15/Result.php
@@ -9,10 +9,10 @@
       echo "<center>";
       echo "<table>";
       while($row = mysql_fetch_row($result)){
-        echo "<tr><td>Marklist</td></tr>";
+        echo "<tr><td><b><u>Marklist</u></b></td></tr>";
         echo "<tr><td>RegNo:</td><td>{$row[0]}</td></tr>";
         echo "<tr><td>Name:</td><td>{$row[1]}</td></tr>";
-        echo "<tr><td>Marks</td></tr>";
+        echo "<tr><td><b><u>Marks</u></b></td></tr>";
         echo "<tr><td>ASP:</td><td>{$row[2]}</td></tr>";
         echo "<tr><td>OS:</td><td>{$row[3]}</td></tr>";
         echo "<tr><td>Network:</td><td>{$row[4]}</td></tr>";

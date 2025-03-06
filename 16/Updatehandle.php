4f1f84ca7d9d9c095ed419994a8c389eca0d193b
Manu Krishna <krishnamanu27112004@gmail.com>
2025-03-06 19:17:16 +0530

    Update Updatehandle.php

--- 16/Updatehandle.php
+++ 16/Updatehandle.php
@@ -1,16 +1,20 @@
 <html>
   <body>
+    <head>
+      <title>Update Account</title>
+    </head>
     <?php
       mysql_connect("localhost", "root", "tiger");
       mysql_select_db("ph");
-      $r=$_POST['accno'];
-      $query = "update account set amount = amount + 240 where accno='$r'";
+      $r=$_POST[ 'accno' ];
+      $query = "UPDATE account SET amount = amount + 240 WHERE accno = '$r'";
       $result=mysql_query($query);
       if(mysql_query_rows() == 1){
+        echo "The specified row has been updated."
       }
       else
       {
-        echo "Invalid entry";
+        echo "Invalid entry!";
       }
     ?>
   </body>
